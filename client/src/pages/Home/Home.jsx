f6614c5dcaddad2456199d584bf3053bacedcaa1
Prince Kumar <princepk7878kumar@gmail.com>
2023-12-17 22:02:20 +0530

    Added basic layout and integrated to homepahe

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import HomeLayout from "../../Layouts/HomeLayout/HomeLayout";
+import homePageMainImage from '../../assets/Images/homePageMainImage.png'
+
+function Home() {
+    return (
+        <HomeLayout>
+        <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 h-[90vh]">
+              <div className="w-1/2 space-y-6">
+                <h1 className="text-5xl font-semibold">Find Out Best <span className="text-yellow-500 font-fold">Online Courses</span></h1>
+                <p className="text-xl text-gray-200">
+                    we have a large Library of courses taught by highly skilled and qualified faculities at a very affortable cost.
+                </p>
+                <div className="space-x-6">
+                    <Link to="/courses">
+                        <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
+                            Explore Courses
+                        </button>
+                    </Link>
+                    <Link to="/contacts">
+                        <button className="border border-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
+                            Contact Us
+                        </button>
+                    </Link>
+                </div>
+              </div>
+
+              <div className="w-1/2 flex items-center justify-center">
+                    <img src={homePageMainImage} alt="Home Page" />
+              </div>
+            </div>
+        </HomeLayout>
+    );
+}
+
+export default Home;
