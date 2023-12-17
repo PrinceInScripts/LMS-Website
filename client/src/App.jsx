3dc33598e09d5ad4140d0215ffd2c6ec3accc101
Prince Kumar <princepk7878kumar@gmail.com>
2023-12-17 20:22:30 +0530

    adding footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
+import Footer from './components/Footer/Footer'
 
 
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>
-        Hello world
-      </h1>
+    
     </>
   )
 }
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import {BsFacebook} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsTwitter} from 'react-icons/bs'
+
+function Footer() {
+
+    const newDate=new Date()
+    const year=newDate.getFullYear()
+    return (
+            <footer className="relative left-0 bottom-0 h-[10vh] py-5 flex flex-col sm:flex-row items-center justify-between sm:px-20 test-white bg-gray-800">
+                   <section className="text-lg">
+                       Copyright {year} | All rights reserved
+                   </section>
+                   <section className="flex items-center justify-center gap-5 test-2xl text-white">
+                       <a href="#" className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
+                            <BsFacebook/>
+                       </a>
+                       <a href="#" className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
+                            <BsInstagram/>
+                       </a>
+                       <a href="#" className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
+                            <BsLinkedin/>
+                       </a>
+                       <a href="#" className='hover:text-yellow-500 transition-all ease-in-out duration-300'>
+                            <BsTwitter/>
+                       </a>
+                   </section>
+                     
+            </footer>
+
+    );
+}
+
+export default Footer;
