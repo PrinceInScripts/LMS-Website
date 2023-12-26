b2a83d7d425fe99a0d10478069b4062f8016691d
Prince Kumar <princepk7878kumar@gmail.com>
2023-12-26 17:57:57 +0530

    Added user profile page completed

--- client/src/pages/User/EditProfile/EditProfile.jsx
+++ client/src/pages/User/EditProfile/EditProfile.jsx
@@ -1,9 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
 import HomeLayout from "../../../Layouts/HomeLayout/HomeLayout";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { getUserData, updateProfile } from "../../../redux/slices/authSlice";
+import { BsPersonCircle } from "react-icons/bs";
 
 function EditProfile() {
     const dispatch=useDispatch()
@@ -20,8 +21,9 @@ function EditProfile() {
         e.preventDefault()
 
         const uploadImage=e.target.files[0]
-        FileReader.readAsDataUrl(uploadImage)
-        FileReader.addEventListener("load",function (){
+        const fileReader = new FileReader();
+      fileReader.readAsDataURL(uploadImage);
+        fileReader.addEventListener("load",function (){
             setData({
                 ...data,
                 previewImage:this.result,
@@ -37,7 +39,7 @@ function EditProfile() {
             [name]:value
         })
     }
-
+    
    async function onFormSubmit(e){
        e.preventDefault()
 
@@ -54,6 +56,8 @@ function EditProfile() {
        const formData=new FormData()
        formData.append("fullName",data.fullName)
        formData.append("avatar",data.avatar)
+
+
        await dispatch(updateProfile([data.userId,formData]))
        await dispatch(getUserData())
 
@@ -61,7 +65,64 @@ function EditProfile() {
     }
     return (
         <HomeLayout>
-            
+            <div className="flex items-center justify-center h-[100vh]">
+              <form
+              onSubmit={onFormSubmit}
+              className="flex flex-col justify-center gap-5 rounded-lg p-4 text-white w-80 h-[26rem] shadow-[0_0_10px_black]"
+              >
+                <h1 className="text-center text-2xl font-semibold">
+                    Edit Profile
+                </h1>
+
+                <label htmlFor="image_uploads" className="cursor-pointer">
+                      {
+                        data.previewImage ? (
+                            <img 
+                            src={data.previewImage}
+                            className="w-20 h-28 rounded-full m-auto"
+                            />
+                        ) :(
+                            <BsPersonCircle className="w-28 h-28 rounded-full m-auto"/>
+                        )
+                      }
+                </label>
+                <input 
+                type="file"
+                onChange={handleImageUpload}
+                id="image_uploads"
+                accept=".jpg, .png, .jpeg, .svg"
+                className="hidden"
+                />
+
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="fullName" className="text-lg font-semibold">
+                       Full name
+                    </label>
+                    <input
+                     required
+                     type="text"
+                     id="fullName"
+                     name="fullName"
+                     placeholder="Enter your name"
+                     value={data.fullName}
+                     className="bg-transparent px-2 py-1 border"
+                     onChange={handleInputChange}
+                    />
+                </div>
+                <button
+                type="submit"
+                className="w-full bg-yellow-500 hover:bg-yellow-600 transition-all ease-in-out duration-300 rounded-sm py-2 cursor-pointer text-center"
+                >
+                 Update Profile
+                </button>
+                <Link to="/user/profile">
+                <p className="link text-green-600 cursor-pointer flex items-center justify-center w-full gap-2 ">
+                    Go back to profile
+                </p>
+                </Link>
+
+              </form>
+            </div>
         </HomeLayout>
     );
 }
