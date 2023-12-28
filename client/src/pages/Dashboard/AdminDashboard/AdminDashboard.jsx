04cf393da4f0967b0c9512ad37114bc0da05e551
Prince Kumar <princepk7878kumar@gmail.com>
2023-12-28 17:20:14 +0530

    Done admin adsboard page

--- client/src/pages/Dashboard/AdminDashboard/AdminDashboard.jsx
+++ client/src/pages/Dashboard/AdminDashboard/AdminDashboard.jsx
@@ -1,10 +1,36 @@
 import React, { useEffect } from 'react';
 import HomeLayout from '../../../Layouts/HomeLayout/HomeLayout';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+} from "chart.js";
+import { Pie, Bar } from "react-chartjs-2";
+import { FaUsers } from "react-icons/fa";
+import { GiMoneyStack } from "react-icons/gi";
+import { FcSalesPerformance } from "react-icons/fc";
+import { BsCollectionPlayFill, BsTrash } from "react-icons/bs";
+import { MdOutlineModeEdit } from "react-icons/md";
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { getAllCourses } from '../../../redux/slices/courseSlice';
+import { deleteCourse, getAllCourses } from '../../../redux/slices/courseSlice';
 import { getStatsData } from '../../../redux/slices/statSlice';
 import { getPaymentRecord } from '../../../redux/slices/razorPaySlice';
+
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title
+);
 
 function AdminDashboard() {
   const dispatch=useDispatch()
@@ -75,7 +101,173 @@ useEffect(() => {
 
   return (
     <HomeLayout>
+     <div className="min-h-[90vh] pt-5 flex flex-col flex-wrap gap-10 text-white">
+     <h1 className="text-center text-3xl font-semibold text-yellow-500">
+          Admin Dashboard
+        </h1>
       
+      <div className="grid grid-cols-2 gap-5 m-auto mx-10">
+      <div className="flex flex-col items-center gap-10 p-5 shadow-lg rounded-md">
+            <div className='w-80 h-80'>
+                <Pie data={userData}/>
+            </div>
+
+            <div className="grid grid-cols-2 gap-5">
+            <div className="flex items-center justify-between py-5 px-5 gap-5 rounded-md shadow-md">
+                    <div className='flex flex-col items-center'>
+                      <p className='font-semibold'>Registered Users</p>
+                      <h3 className='text-4xl font-bold'>{allUsersCount}</h3>
+                    </div>     
+                    <FaUsers className='text-yellow-500 text-5xl'/>           
+              </div>
+
+              <div className="flex items-center justify-between py-5 px-5 gap-5 rounded-md shadow-md">
+                <div className="flex flex-col items-center">
+                  <p className="font-semibold">Subscribed Users</p>
+                  <h3 className="text-4xl font-bold">{subscribedUsersCount}</h3>
+                </div>
+                <FaUsers className="text-green-500 text-5xl" />
+              </div>
+            </div>
+        </div>
+
+        <div className="flex flex-col items-center gap-10 p-5 shadow-lg rounded-md">
+           <div className="h-80 relative w-full">
+              <Bar className="absolute bottom-0 h-80 w-full" data={salesData} />
+            </div> 
+
+            <div className="grid grid-cols-2 gap-5">
+            <div className="flex items-center justify-between py-5 px-5 gap-5 rounded-md shadow-md">
+                <div className="flex flex-col items-center">
+                  <p className="font-semibold">Subscriptions Count</p>
+                  <h3 className="text-4xl font-bold">{allPayments?.count}</h3>
+                </div>
+                <FcSalesPerformance className="text-yellow-500 text-5xl" />
+              </div>  
+
+              <div className="flex items-center justify-between py-5 px-5 gap-5 rounded-md shadow-md">
+              <div className="flex flex-col items-center">
+                  <p className="font-semibold">Total Revenue</p>
+                  <h3 className="text-4xl font-bold">
+                    {allPayments?.count * 499}
+                  </h3>
+                </div>
+                <GiMoneyStack className="text-green-500 text-5xl" />           
+              </div>           
+            </div>       
+        </div>
+      </div>
+
+
+      <div className="mx-[10%] w-[80%] self-center flex flex-col items-center justify-center gap-10 mb-10">
+           <div className='flex w-full items-center justify-between'>
+           <h1 className="text-center text-3xl font-semibold">
+              Courses Overview
+            </h1>
+
+            <button
+              onClick={() => {
+                navigate("/course/create", {
+                  state: {
+                    initialCourseData: {
+                      newCourse: true,
+                      title: "",
+                      category: "",
+                      createdBy: "",
+                      description: "",
+                      thumbnail: undefined,
+                      previewImage: "",
+                    },
+                  },
+                });
+              }}
+              className="w-fit bg-yellow-500 hover:bg-yellow-600 transition-all ease-in-out duration-300 rounded py-2 px-4 font-semibold text-lg cursor-pointer"
+            >
+              Create New Course
+            </button>
+            </div>   
+
+            <table className='table overflow-x-scroll'>
+               <thead>
+                  <tr>
+                  <th>S No.</th>
+                  <th>Course Title</th>
+                  <th>Course Category</th>
+                  <th>Instructor</th>
+                  <th>Total Lectures</th>
+                  <th>Course Description</th>
+                  <th>Actions</th>
+                  </tr>
+               </thead>
+
+               <tbody>
+                {myCourses?.map((element,index)=>{
+                  return (
+                    <tr key={element?._id}>
+                       <td>{index+1}</td>
+                       <td>
+                          <textarea 
+                             readOnly
+                             className="w-40 h-auto bg-transparent resize-none"
+                             value={element?.title}
+                          ></textarea>
+                       </td>
+                       <td>{element?.category}</td>
+                      <td>{element?.createdBy}</td>
+                      <td>{element?.numberOfLectures}</td>
+                      <td className="max-w-28 overflow-hidden text-ellipsis whitespace-nowrap">
+                      <textarea
+                        readOnly
+                        className="w-80 h-auto bg-transparent resize-none"
+                        value={element?.description}
+                      ></textarea>
+                    </td>
+
+                    <td className="flex items-center gap-4">
+                    <button
+                        onClick={() =>
+                          navigate("/course/create", {
+                            state: {
+                              initialCourseData: {
+                                newCourse: false,
+                                ...element,
+                              },
+                            },
+                          })
+                        }
+                        className="bg-yellow-500 hover:bg-yellow-600 transition-all ease-in-out duration-300 text-xl py-2 px-4 rounded-md font-bold"
+                      >
+                        <MdOutlineModeEdit />
+                      </button>
+
+                      <button
+                        onClick={() => handleCourseDelete(element._id)}
+                        className="bg-red-500 hover:bg-red-600 transition-all ease-in-out duration-30 text-xl py-2 px-4 rounded-md font-bold"
+                      >
+                        <BsTrash />
+                      </button>
+
+                      <button
+                        onClick={() =>
+                          navigate("/course/displaylectures", {
+                            state: { ...element },
+                          })
+                        }
+                        className="bg-green-500 hover:bg-green-600 transition-all ease-in-out duration-30 text-xl py-2 px-4 rounded-md font-bold"
+                      >
+                        <BsCollectionPlayFill />
+                      </button>
+
+                    </td>
+                    </tr>
+                  )
+                })}
+               </tbody>
+            
+            </table>     
+      </div>
+
+      </div>
     </HomeLayout>
   );
 }
--- client/src/redux/slices/courseSlice.js
+++ client/src/redux/slices/courseSlice.js
@@ -47,7 +47,53 @@ export const createNewCourse=createAsyncThunk("/course/create",async (data)=>{
     }
 })
 
+export const deleteCourse = createAsyncThunk("/course/delete", async (id) => {
+    try {
+      const res = axiosInstance.delete(`courses/${id}`);
+  
+      toast.promise(res, {
+        loading: "Deleting the course...",
+        success: "Courses deleted successfully",
+        error: "Failed to delete course",
+      });
+  
+      const response = await res;
+  
+      return response.data;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  });
 
+  export const updateCourse = createAsyncThunk("/course/update", async (data) => {
+    try {
+      const formData = new FormData();
+      formData.append("title", data.title);
+      formData.append("category", data.category);
+      formData.append("createdBy", data.createdBy);
+      formData.append("description", data.description);
+      
+      const res = axiosInstance.put(`/courses/${data.id}`, {
+        title: data.title,
+        category: data.category,
+        createdBy: data.createdBy,
+        description: data.description,
+      });
+  
+      toast.promise(res, {
+        loading: "Updating the course...",
+        success: "Course updated successfully",
+        error: "Failed to update course",
+      });
+  
+      const response = await res;
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.response?.data?.message);
+    }
+  });
+  
 
 
 
